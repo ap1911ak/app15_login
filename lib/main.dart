25ac573b3817c8869f9aff4fab1994a0f5dd4b77
CrocodileSoy <66096745+ap1911ak@users.noreply.github.com>
2025-07-24 01:40:48 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'appbar.dart';
+import 'share.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +11,39 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      home: HomePage(),
     );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    var _text = (!Share.isLoggedIn) ? 'Please Login' : 'You Loged in';
+
+    var _icon = (!Share.isLoggedIn) ? Icons.lock_rounded : Icons.check;
+
+    Share.updateState = (value) {
+      setState(() {
+        Share.isLoggedIn = value;
+      });
+    };
+
+    return Scaffold(
+        appBar: buildAppbar(context, 'App Login Home'),
+        body: Container(
+            alignment: Alignment.topCenter,
+            padding: EdgeInsets.only(top: 50, left: 20, right: 20),
+            child: Column(children: [
+              Icon(_icon, size: 64),
+              SizedBox(height: 30),
+              Text(_text, textScaler: TextScaler.linear(1.7))
+            ])));
   }
 }
